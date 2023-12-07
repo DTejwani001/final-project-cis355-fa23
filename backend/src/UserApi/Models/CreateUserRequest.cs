2af3cd3e492edcb84a7f8ed9b8391fc00c920014
OjashwiUprety <uprety003@gannon.edu>
2023-12-06 23:52:44 -0500

    new commit1

--- backend/src/UserApi/Models/CreateUserRequest.cs
+++ backend/src/UserApi/Models/CreateUserRequest.cs
@@ -10,26 +10,33 @@ public class CreateUserRequest
     /// <summary>
     /// The first name of the user.
     /// </summary>
-    [Required]
+    [Required ]
+    [StringLength(255)]
     public string FirstName { get; set; } = null!;
 
     /// <summary>
     /// The last name of the user.
     /// </summary>
     [Required]
+     [StringLength(255)]
     public string LastName { get; set; } = null!;
 
     /// <summary>
     /// The username of the user.
     /// </summary>
     [Required]
+     [StringLength(255)]
     public string Username { get; set; } = null!;
 
     /// <summary>
     /// The email address of the user.
     /// </summary>
-    [Required]
-    [EmailAddress]
+    //[Required]
+    //[EmailAddress]
+
+     [Required(ErrorMessage = "Email address is required.")]
+     [EmailAddress(ErrorMessage = "Invalid email address.")]
+      [StringLength(255)]
     public string Email { get; set; } = null!;
 
     /// <summary>
@@ -37,7 +44,7 @@ public class CreateUserRequest
     /// </summary>
     [Required]
     //[MinLength(8)]
-    [RegularExpression("/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}/g", ErrorMessage = "Password must meet requirements")]
+    [RegularExpression("/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8}/g", ErrorMessage = "Password must be at least 8 characters long and include at least one lowercase letter, one uppercase letter, and one digit.")]
     public string Password { get; set; } = null!;
 
     /// <summary>
